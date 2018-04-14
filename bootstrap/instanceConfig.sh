8caec23ca40bda7effbe0c36457f5acd151b1ca8
Stephen Montsaroff <smontsaroff@luxoft.com>
2018-04-14 00:07:21 +0000

    Updated a bootstrapper script to get the latest version of hyperledger, and only get the binaries.

--- bootstrap/instanceConfig.sh
+++ bootstrap/instanceConfig.sh
@@ -43,4 +43,4 @@ apt-get install -y golang-goprotobuf-dev
 #--npm install npm@latest -g  
 #--npm i -g n  
 #--n stable  
-curl -sSL https://goo.gl/kFFqh5 | (cd /usr/local; bash -   )
+curl -sSL https://goo.gl/6wtTN5 | (cd /usr/local;bash -s - -s -d)
